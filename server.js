6cbaf5c6c387c8457bab3acb015f43565bcc70e3
Nino Peters <nino@salonid.com>
2025-04-25 16:10:29 +0200

    Fix: correcte SOAP endpoint KBO

--- server.js
+++ server.js
@@ -1,42 +1,64 @@
-// server.js
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 
+// CORS toevoegen om aanroepen vanuit Google Apps Script toe te staan
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/lookup", async (req, res) => {
   const { ondernemingsnummer } = req.body;
-
+  
   if (!ondernemingsnummer) {
     return res.status(400).json({ error: "ondernemingsnummer is required" });
   }
-
-  const soapEnvelope = `
-  <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:kbo="http://economie.fgov.be/kbo">
-    <soapenv:Header/>
-    <soapenv:Body>
-      <kbo:getOnderneming>
-        <kbo:ondernemingsnummer>${ondernemingsnummer}</kbo:ondernemingsnummer>
-      </kbo:getOnderneming>
-    </soapenv:Body>
-  </soapenv:Envelope>`;
+  
+  // Correcte SOAP envelope formatteren
+  const soapEnvelope = `<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:kbo="http://economie.fgov.be/kbopub">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <kbo:getEnterpriseByEnterpriseNumber>
+      <kbo:enterpriseNumber>${ondernemingsnummer}</kbo:enterpriseNumber>
+    </kbo:getEnterpriseByEnterpriseNumber>
+  </soapenv:Body>
+</soapenv:Envelope>`;
 
   const auth = Buffer.from("wsot0761:vbV5pKuFMFL7nTTpwC4KhPvV").toString("base64");
-
+  
   try {
-    const response = await axios.post("https://kbopub.economie.fgov.be/kbopubwebservice/kbo/KBOService", soapEnvelope, {
-      headers: {
-        "Content-Type": "text/xml;charset=UTF-8",
-        "Authorization": `Basic ${auth}`,
-      },
-    });
-
+    const response = await axios.post(
+      "https://kbopub.economie.fgov.be/kbopubwebservice/kbo/KBOService",
+      soapEnvelope,
+      {
+        headers: {
+          "Content-Type": "text/xml;charset=UTF-8",
+          "SOAPAction": "",
+          "Authorization": `Basic ${auth}`,
+        },
+      }
+    );
+    
     res.send({ xml: response.data });
   } catch (error) {
-    res.status(500).send({ error: error.toString() });
+    console.error("KBO API Error:", error.message);
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+    }
+    res.status(500).send({ 
+      error: error.toString(),
+      details: error.response ? error.response.data : null
+    });
   }
+});
+
+// Voeg een eenvoudige test-endpoint toe
+app.get("/", (req, res) => {
+  res.send("KBO proxy server is running!");
 });
 
 const port = process.env.PORT || 3000;
